79aab7f593fbd374722f17b938c5da9825c46f2e
Daluxea <73659254+dddddavid02@users.noreply.github.com>
2021-11-24 11:39:09 +0100

    Add files via upload

---
+++ negozio-sportivo/database.sql
@@ -0,0 +1,36 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versione server:              10.4.21-MariaDB - mariadb.org binary distribution
+-- S.O. server:                  Win64
+-- HeidiSQL Versione:            11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dump della struttura del database negozio-sportivo
+CREATE DATABASE IF NOT EXISTS `negozio-sportivo` /*!40100 DEFAULT CHARACTER SET armscii8 COLLATE armscii8_bin */;
+USE `negozio-sportivo`;
+
+-- Dump della struttura di tabella negozio-sportivo.articoli
+CREATE TABLE IF NOT EXISTS `articoli` (
+  `id_articolo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) COLLATE armscii8_bin DEFAULT NULL,
+  `quantita` int(11) DEFAULT NULL,
+  `prezzo` float DEFAULT NULL,
+  PRIMARY KEY (`id_articolo`)
+) ENGINE=InnoDB DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+
+-- Dump dei dati della tabella negozio-sportivo.articoli: ~0 rows (circa)
+/*!40000 ALTER TABLE `articoli` DISABLE KEYS */;
+/*!40000 ALTER TABLE `articoli` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;

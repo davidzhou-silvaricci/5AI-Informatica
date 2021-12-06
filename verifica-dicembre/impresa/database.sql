c72f29bbdb80fc0f1099e1a6ec80aa8c5338de64
dddddavid02 <73659254+dddddavid02@users.noreply.github.com>
2021-12-06 13:32:39 +0100

    Verifica

---
+++ verifica-dicembre/impresa/database.sql
@@ -0,0 +1,44 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versione server:              10.4.21-MariaDB - mariadb.org binary distribution
+-- S.O. server:                  Win64
+-- HeidiSQL Versione:            11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dump della struttura del database impresa
+CREATE DATABASE IF NOT EXISTS `impresa` /*!40100 DEFAULT CHARACTER SET armscii8 COLLATE armscii8_bin */;
+USE `impresa`;
+
+-- Dump della struttura di tabella impresa.interventi
+CREATE TABLE IF NOT EXISTS `interventi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tecnico` varchar(50) COLLATE armscii8_bin NOT NULL DEFAULT '',
+  `durata` int(11) NOT NULL DEFAULT 0,
+  `parcella` int(11) NOT NULL DEFAULT 0,
+  `scontato` int(11) NOT NULL,
+  `nota` varchar(50) COLLATE armscii8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+
+-- Dump dei dati della tabella impresa.interventi: ~7 rows (circa)
+/*!40000 ALTER TABLE `interventi` DISABLE KEYS */;
+INSERT INTO `interventi` (`id`, `tecnico`, `durata`, `parcella`, `scontato`, `nota`) VALUES
+	(10, 'Mario', 123, 124, 87, 'awd'),
+	(11, 'Luca', 3215, 112, 78, ''),
+	(12, 'Mario', 12, 12, 0, ''),
+	(13, 'Mario', 12, 215, 0, ''),
+	(14, 'Mario', 2, 10553, 0, '');
+/*!40000 ALTER TABLE `interventi` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;

e47c70aec49e9f8ddff7c6193bb8bd1e96096ce8
dddddavid02 <73659254+dddddavid02@users.noreply.github.com>
2021-12-07 22:14:28 +0100

    Conclusione del registro donazioni

--- registro-donazioni/Url.php
+++ registro-donazioni/Url.php
@@ -4,8 +4,8 @@ class Url
 {
     const HOME = "index.php";
     const ADD = "form.php";
+    const VIEW = "view.php";
     const UPDATE = "update.php";
-    const RESET = self::HOME . "?reset=";
 
     public static function toHome()
     {
@@ -17,9 +17,20 @@ class Url
         return self::ADD;
     }
 
-    public static function toReset()
-    {
-        return self::RESET;
+    public static function urlView($id) {
+        return self::VIEW . "?id=$id";
+    }
+
+    public static function urlEditEnte($id, $ente) {
+        return self::VIEW . "?id=$id&ente=$ente";
+    }
+
+    public static function urlFilterEnte($ente) {
+        return self::HOME . "?filter=$ente&by=ente";
+    }
+
+    public static function urlFilterImporto($importo) {
+        return self::HOME . "?filter=$importo&by=importo";
     }
 
     public static function urlDelete($id) {
--- registro-donazioni/index.php
+++ registro-donazioni/index.php
@@ -2,6 +2,15 @@
 
 include("autoloader.php");
 
+$donazione = new Donazione();
+
+if (isset($_POST["submit"]))
+    $donazione->add($_POST);
+
+if(isset($_GET["filter"]) && isset($_GET["by"]))
+    $lista = $donazione->getFilteredList($_GET["filter"], $_GET["by"]);
+else $lista = Donazione::getDonazioni();
+
 ?>
 
 <!DOCTYPE html>
@@ -17,19 +26,57 @@ include("autoloader.php");
 
 <body>
     <div class="uk-container uk-container-xsmall uk-padding-large">
-        <div class="uk-flex">
-            <a class="uk-button uk-button-default uk-button-large uk-width-1-1" href="<?= Url::toReset() ?>">Svuota</a>
+        <div class="uk-flex uk-margin-medium-bottom">
+            <div class="uk-inline uk-width-1-1">
+                <button class="uk-button uk-button-default uk-button-large uk-width-1-1" type="button">Filtra</button>
+                <div class="uk-width-1-1" uk-dropdown>
+                    <ul class="uk-nav uk-dropdown-nav">
+                        <li><a href="<?= Url::toHome() ?>">Tutti</a></li>
+                        <li class="uk-nav-header">Ente</li>
+                        <?php foreach (Donazione::ENTI as $k => $v) : ?>
+                            <li><a href="<?= Url::urlFilterEnte($k) ?>"><?= $v ?></a></li>
+                        <?php endforeach; ?>
+                        <li class="uk-nav-header">Importo (€)</li>
+                        <li><a href="<?= Url::urlFilterImporto(100) ?>">> 100</a></li>
+                    </ul>
+                </div>
+            </div>
             <a class="uk-button uk-button-primary uk-button-large uk-width-1-1 uk-margin-left" href="<?= Url::toForm() ?>">Aggiungi</a>
         </div>
-        <?php // ciclo ?>
-            <div class="uk-card uk-card-default uk-card-body uk-margin">
-                <div class="uk-card-badge uk-label"></div>
-                <h3 class="uk-card-title"></h3>
-                <p></p>
-                <a href="#" class="uk-button uk-button-default uk-margin-small-left"></a>
-            </div>
-        <?php // fine ciclo ?>
-        <?php if (true) : ?>
+        <table class="uk-table uk-table-middle uk-table-responsive uk-table-hover uk-table-striped uk-table-divider">
+            <thead>
+                <tr>
+                    <th class="uk-table-shrink">ID</th>
+                    <th class="uk-width-small">Importo (€)</th>
+                    <th>Ente</th>
+                    <th>Anno</th>
+                    <th class="uk-table-expand">Nota</th>
+                    <th class="uk-table-shrink">Azione</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($obj = $lista->fetch_object()) : ?>
+                    <!--<div class="uk-card uk-card-default uk-card-body uk-margin-medium">
+                        <div class="uk-card-badge">
+                            <span class="uk-label">€ <?= number_format((float) $obj->importo, 2, ',', '.') ?></span>
+                            <span class="uk-label"><?= $obj->anno ?></span>
+                        </div>
+                        <h3 class="uk-card-title"><?= Donazione::getEnte($obj->ente) ?></h3>
+                        <p class="uk-margin-medium-bottom"><?= $obj->nota !== "" ? $obj->nota : "<i>Nessuna nota</i>" ?></p>
+                        <a href="<?= Url::urlView($obj->id) ?>" class="uk-button uk-button-primary">Dettagli</a>
+                    </div>-->
+                    <tr>
+                        <td><?= $obj->id ?></td>
+                        <td><?= number_format((float) $obj->importo, 2, ',', '.') ?></td>
+                        <td><?= Donazione::getEnte($obj->ente) ?></td>
+                        <td><?= $obj->anno ?></td>
+                        <td class="uk-text-truncate"><?= $obj->nota !== "" ? $obj->nota : "<i>Nessuna nota</i>" ?></td>
+                        <td><a href="<?= Url::urlView($obj->id) ?>" class="uk-button uk-button-default">Dettagli</a></td>
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+        <?php if ($lista->num_rows === 0) : ?>
             <div class="uk-card uk-card-default uk-card-body uk-margin uk-text-center">
                 <p>Nessuna donazione da visualizzare</p>
             </div>
